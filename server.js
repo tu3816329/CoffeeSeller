71b87ded77444cc564a49c09a424393212732219
tu3816329 <tu3816329@gmail.com>
2017-02-22 17:05:31 +0700

    Add files via upload

--- server.js
+++ server.js
@@ -9,10 +9,11 @@
 //app.set('port', process.env.PORT || 3000);
 //------------------------------------------------
 //var SELECT_BY_PRODUCT_NAME_QUERY
-var SELECT_ALL_DETAIL_QUERY = "Select a.name,a.price,a.FoodType,b.name from ( select a.name,a.price,b.name as FoodType,b.product_type_id from tbl_product a INNER JOIN tbl_DetailProductType b ON a.Type_id=b.id ) a INNER JOIN tbl_ProductType b ON a.product_type_id=b.id";
+var SELECT_ALL_DETAIL_QUERY = "Select a.name,a.price,a.description,a.imagelink,a.FoodType,b.name,a.id,a.product_type_id from ( select a.*,b.name as FoodType,b.product_type_id from tbl_product a INNER JOIN tbl_DetailProductType b ON a.Type_id=b.id ) a INNER JOIN tbl_ProductType b ON a.product_type_id=b.id";
 var SELECT_BY_PRICE_QUERY = "select b.name as Product,a.name,a.Type_Name as type,a.price from tbl_producttype b, ( select a.name,a.price,b.name as Type_Name,b.product_type_id from tbl_product  a inner join tbl_detailproducttype b  ON a.type_id=b.id where a.price=$1 ) a where a.product_type_id=b.id ";
-var SELECT_PRODUCT_TYPE_QUERY = "Select name From tbl_ProductType";
-var SELECT_BY_PRODUCT_TYPE_QUERY = "Select name From tbl_ProductType";
+var SELECT_PRODUCT_TYPE_QUERY = "Select * From tbl_ProductType";
+
+var SELECT_DETAIL_PRODUCT_TYPE_QUERY = "Select name From tbl_DetailProductType";
 var http = require('http');
 var url = require('url');
 var sQuerry = require('querystring');
@@ -35,7 +36,7 @@ var app = express();
  user: '',
  password: '',
  ssl: true,
- poolSize: 20
+ poolSize: 25
  };
  var db=pgPromise(conConfig);
  */
@@ -49,41 +50,49 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
 //------------------SupportFunction-----------------------------------------
-function getProductTypeID(name) {
-    return db.oneOrNone(SELECT_PRODUCT_TYPE_QUERY + " Where name=$1", name).then(function (row) {
-        return row;
-    });
-}
 
 //------------------Handle Post Request--------------------------------------
 app.post('/webhook', function (request, response) {
 //    console.log(request.body);
     var jsBody = request.body;
     response.writeHeader(200, {'Content-type': "Application/json"});
-    var content;
+    var content = "";
     //----------------------Handle Show Menu Request-------------------------
     if (jsBody.result.action.toString().toUpperCase() === "show_menu".toString().toUpperCase()) {
-        if (jsBody.result.parameters.Type != "") {
-            var id = getProductTypeID(jsBody.result.parameters.Type.toString());
-            if (id != null) {
-                db.many(SELECT_ALL_DETAIL_QUERY).then(function (row) {
-                    var display = "";
-                    var speech = "Here s your menu";
-                    var text = speech;
-                    var content = {'speech': speech,
-                        'displayText': text,
-                        'data': row,
-                        'contextOut': [], 'source': "Thien Tu", 'followupEvent': {
+        if (jsBody.result.parameters.Type !== "") {
+            db.one(SELECT_PRODUCT_TYPE_QUERY + " where name LIKE ${name}", {name: jsBody.result.parameters.Type}).then(function (data) {
+                var id = data.id;
+                if (id !== null) {
+                    db.many(SELECT_DETAIL_PRODUCT_TYPE_QUERY + " where product_type_id=${id} ", {id: id}).then(function (row) {
+
+                        console.log("row:" + row);
+                        var display = "";
+                        var speech = "We have \\n";
+                        for (var i = 0; i < row.length; i++) {
+                            if (i == (row[i].length - 1)) {
+                                speech += "And " + row.name + " \\n";
+                            } else {
+                                speech += row[i].name + " \\n";
+                            }
                         }
-                    };
-                    response.write(JSON.stringify(content));
-                    console.log("Send response: " + JSON.stringify(content));
-                    response.end();
-                }).catch(function (error) {
-                    if (error)
-                        throw error;
-                });
-            }
+                        var text = speech;
+                        var content = {'speech': speech,
+                            'displayText': text,
+                            'data': row,
+                            'contextOut': [], 'source': "Thien Tu", 'followupEvent': {
+                            }
+                        };
+                        response.write(JSON.stringify(content));
+                        console.log("Send response: " + JSON.stringify(content));
+                        response.end();
+                    }).catch(function (error) {
+                        if (error)
+                            throw error;
+                    });
+                } else {
+
+                }
+            });
 
         }
 
